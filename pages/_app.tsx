e7e17773dc558bce755c2f5ad3787d3c89dc645d
SatangBudsai <mastersatang+github@gmail.com>
2023-06-19 19:07:16 +0700

    add InjectTailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,20 +9,24 @@ import theme from "@/config/materialUI/theme";
 import Nprogress from '@/components/Nprogress';
 
 import type { AppProps } from 'next/app'
+import InjectTailwind from '@/pages/InjectTailwind';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-          <title>AroundTheCode</title>
-        </Head>
-        <Nprogress>
-          <Component {...pageProps} />
-        </Nprogress>
+        <InjectTailwind>
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <title>AroundTheCode</title>
+          </Head>
+          <Nprogress>
+            <Component {...pageProps} />
+          </Nprogress>
+        </InjectTailwind>
       </ThemeProvider>
     </QueryClientProvider>
   )
